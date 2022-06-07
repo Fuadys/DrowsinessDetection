aa066f9493544c1751c4336f62d5cc2cb8a660e6
Fuady <fadhlanf10@gmail.com>
2022-06-07 21:05:54 +0700

    first commit

---
+++ webMain.py
@@ -0,0 +1,194 @@
+import numpy as np
+import cv2
+import streamlit as st
+from tensorflow import keras
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import os
+from keras.models import load_model
+from pygame import mixer
+import time
+
+mixer.init()
+sound = mixer.Sound('alarm.wav')
+
+lbl=['Close','Open']
+
+model = load_model('models/cnncat2.h5')
+path = os.getcwd()
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+count=0
+score=0
+thicc=2
+rpred=[99]
+lpred=[99]
+
+try:
+    face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
+    leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
+    reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+
+except Exception:
+    st.write("Error loading masszeh")
+
+while(True):
+    ret, frame = cap.read()
+    height,width = frame.shape[:2] 
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
+    left_eye = leye.detectMultiScale(gray)
+    right_eye =  reye.detectMultiScale(gray)
+
+    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
+
+    for (x,y,w,h) in right_eye:
+        r_eye=frame[y:y+h,x:x+w]
+        count=count+1
+        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+        r_eye = cv2.resize(r_eye,(24,24))
+        r_eye= r_eye/255
+        r_eye=  r_eye.reshape(24,24,-1)
+        r_eye = np.expand_dims(r_eye,axis=0)
+        rpred = np.argmax(model.predict(r_eye),axis=-1)
+        if(rpred[0]==1):
+            lbl='Open' 
+        if(rpred[0]==0):
+            lbl='Closed'
+        break
+
+    for (x,y,w,h) in left_eye:
+        l_eye=frame[y:y+h,x:x+w]
+        count=count+1
+        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+        l_eye = cv2.resize(l_eye,(24,24))
+        l_eye= l_eye/255
+        l_eye=l_eye.reshape(24,24,-1)
+        l_eye = np.expand_dims(l_eye,axis=0)
+        lpred = np.argmax(model.predict(l_eye),axis=-1)
+        if(lpred[0]==1):
+            lbl='Open'   
+        if(lpred[0]==0):
+            lbl='Closed'
+        break
+
+    if(rpred[0]==0 and lpred[0]==0):
+        score=score+1
+        cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    # if(rpred[0]==1 or lpred[0]==1):
+    else:
+        score=score-1
+        cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    
+        
+    if(score<0):
+        score=0   
+    cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    if(score>15):
+        #person is feeling sleepy so we beep the alarm
+        cv2.imwrite(os.path.join(path,'image.jpg'),frame)
+        try:
+            sound.play()
+            
+        except:  # isplaying = False
+            pass
+        if(thicc<16):
+            thicc= thicc+2
+        else:
+            thicc=thicc-2
+            if(thicc<2):
+                thicc=2
+        cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc) 
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+#load face
+# class VideoTransformer(VideoTransformerBase):
+#     def transform(self, frame):
+#         img = frame.to_ndarray(format="bgr24")
+
+#         # #image gray
+#         # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         # faces = face_cascade.detectMultiScale(
+#         #     image=img_gray, scaleFactor=1.3, minNeighbors=5)
+#         # for (x, y, w, h) in faces:
+#         #     cv2.rectangle(img=img, pt1=(x, y), pt2=(
+#         #         x + w, y + h), color=(255, 0, 0), thickness=2)
+#         #     roi_gray = img_gray[y:y + h, x:x + w]
+#         #     roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+#         #     if np.sum([roi_gray]) != 0:
+#         #         roi = roi_gray.astype('float') / 255.0
+#         #         roi = img_to_array(roi)
+#         #         roi = np.expand_dims(roi, axis=0)
+#         #         prediction = classifier.predict(roi)[0]
+#         #         maxindex = int(np.argmax(prediction))
+#         #         finalout = emotion_dict[maxindex]
+#         #         output = str(finalout)
+#         #     label_position = (x, y)
+#         #     cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+#         return img
+
+def main():
+    # Face Analysis Application #
+    st.title("Real Time Face Emotion Detection Application")
+    activiteis = ["Home", "Webcam Face Detection", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activiteis)
+    st.sidebar.markdown(
+        """ Developed by Mohammad Juned Khan    
+            Email : Mohammad.juned.z.khan@gmail.com  
+            [LinkedIn] (https://www.linkedin.com/in/md-juned-khan)""")
+    if choice == "Home":
+        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                            <h4 style="color:white;text-align:center;">
+                                            Face Emotion detection application using OpenCV, Custom CNN model and Streamlit.</h4>
+                                            </div>
+                                            </br>"""
+        st.markdown(html_temp_home1, unsafe_allow_html=True)
+        st.write("""
+                 The application has two functionalities.
+
+                 1. Real time face detection using web cam feed.
+
+                 2. Real time face emotion recognization.
+
+                 """)
+    elif choice == "Webcam Face Detection":
+        st.header("Webcam Live Feed")
+        st.write("Click on start to use webcam and detect your face emotion")
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    elif choice == "About":
+        st.subheader("About this app")
+        html_temp_about1= """<div style="background-color:#6D7B8D;padding:10px">
+                                    <h4 style="color:white;text-align:center;">
+                                    Real time face emotion detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
+                                    </div>
+                                    </br>"""
+        st.markdown(html_temp_about1, unsafe_allow_html=True)
+
+        html_temp4 = """
+                             		<div style="background-color:#98AFC7;padding:10px">
+                             		<h4 style="color:white;text-align:center;">This Application is developed by Mohammad Juned Khan using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at Mohammad.juned.z.khan@gmail.com. </h4>
+                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
+                             		</div>
+                             		<br></br>
+                             		<br></br>"""
+
+        st.markdown(html_temp4, unsafe_allow_html=True)
+
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
